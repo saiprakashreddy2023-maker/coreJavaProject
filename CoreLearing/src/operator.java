0eb0300d1310fb2b186c99b81568e2fa03236945
saiprakashreddy2023-maker <saiprakashreddy2023@gmail.com>
2025-12-20 22:07:13 +0530

    pull

---
+++ CoreLearing/src/operator.java
@@ -0,0 +1,12 @@
+public class operator {
+    public static void main(String[] args){
+    int a =20;
+    int b =30;
+        if(a<b){
+            System.out.println("print true");
+        }
+        else{
+            System.out.println("not equal");
+        }
+    }
+}
