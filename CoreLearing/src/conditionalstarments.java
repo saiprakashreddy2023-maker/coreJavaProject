f48983d83b120dd12eb6c86a9845006308bbbc4d
saiprakashreddy2023-maker <saiprakashreddy2023@gmail.com>
2025-12-22 15:16:46 +0530

    pull

--- CoreLearing/src/conditionalstarments.java
+++ CoreLearing/src/conditionalstarments.java
@@ -1,16 +1,16 @@
 public class conditionalstarments {
     public static void main(String[] args) {
         String[] names = {"sai", "reddy", "prakash"};
-        int[] marks = {65, 70, 72};
+        int[] marks = {60, 95, 90};
         char grade;
         System.out.println("Names amd marks");
         System.out.println("-----------------------");
         for (int t = 0; t < names.length; t++) {
             if (marks[t] >= 95) {
                 grade = 'a';
-            } else if (marks[t] >= 80) {
+            } else if (marks[t] > 80) {
                 grade = 'b';
-            } else if (marks[t] >= 60) {
+            } else if (marks[t] >60) {
                 grade = 'c';
             } else {
                 grade = 'f';
